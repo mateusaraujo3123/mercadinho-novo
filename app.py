a19333e162c7fcef7ef8173cb0b178a52de20cc2
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-11 21:46:08 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+# inicio
+import streamlit as st
+from services.google_macro import ler_dados
+from pages.dashboard import mostrar_dashboard
+from pages.clientes import mostrar_clientes
+from pages.produtos import mostrar_produtos
+
+st.set_page_config(page_title="Mercadinho Pro", layout="wide")
+
+# Carregar dados
+df_devedores = ler_dados("Clientes")
+df_produtos = ler_dados("Produtos")
+
+# Menu lateral
+menu = st.sidebar.radio("Menu", ["Dashboard", "Gestão de Fiados", "Produtos"])
+
+if menu == "Dashboard":
+    mostrar_dashboard(df_devedores, df_produtos)
+elif menu == "Gestão de Fiados":
+    mostrar_clientes(df_devedores)
+elif menu == "Produtos":
+    mostrar_produtos(df_produtos)
